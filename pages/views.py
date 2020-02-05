15ff25e22d3af520a2331f9afe39aa1a31e0ff9d
wmiler23 <wmiler23@gmail.com>
2020-02-04 19:28:35 -0500

    adding more files

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,7 @@ from django.http import  HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, '/templates/pages/index.html')
+    return render(request, 'templates/pages/index.html')
 
 def about(request):
-    return render(request, '/templates/pages/about.html')
+    return render(request, 'templates/pages/about.html')
